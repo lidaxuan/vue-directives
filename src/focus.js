367852df365cdf85f3b759031796cdb8f3e7c5b0
lijixuan <15201002062@163.com>
2021-11-16 10:42:11 +0800

    feat: v-focus

--- src/focus.js
+++ src/focus.js
@@ -5,9 +5,22 @@
  * @Date: 2021-08-27 14:55:48
  * @FilePath: /vue-directives/src/focus.js
  */
+const findEle = (parent, type) => {
+  return parent.tagName.toLowerCase() === type ? parent : parent.querySelectorAll(type)
+}
+
 export default {
   inserted: function (el) {
     // 聚焦元素
-    el.focus()
+    if (el.tagName == 'INPUT') {
+      el.focus();
+      return;
+    }
+    if (el.tagName == 'DIV') {
+      let nodes = findEle(el, 'input');
+      if (nodes && nodes.length) {
+        nodes[0].focus();
+      }
+    }
   }
 }
